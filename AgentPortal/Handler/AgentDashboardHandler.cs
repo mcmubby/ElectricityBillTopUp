17c0111b241aa04cf2b279a436bf401ac251eb8a
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-06-14 11:11:26 +0100

    Release

--- AgentPortal/Handler/AgentDashboardHandler.cs
+++ AgentPortal/Handler/AgentDashboardHandler.cs
@@ -6,7 +6,7 @@ namespace AgentPortal.Handler
     public class AgentDashboardHandler
     {
         //Agent self-service
-        //Custome
+        //Custom
 
         public static void SelectService()
         {
